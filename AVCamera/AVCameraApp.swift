8e3341b5f84ace1e2f1d5ee720bc7a24f801683a
Alex Nagy <therebeloper@gmail.com>
2021-08-13 12:30:04 +0300

    Initial Commit

---
+++ AVCamera/AVCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVCameraApp.swift
+//  AVCamera
+//
+//  Created by Alex Nagy on 13.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct AVCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AVCamera
+//
+//  Created by Alex Nagy on 13.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
